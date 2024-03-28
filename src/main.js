1f4c82e9cf521853a4a13133e61a128578cfb26a
szhanggg <stevez9101@gmail.com>
2024-03-27 23:56:19 -0400

    added mbmt and mbit as well as removing animations on mobile

--- src/main.js
+++ src/main.js
@@ -29,127 +29,127 @@ if (window.innerWidth > 768) {
       lenis.scrollTo(targetId);
     });
   });
+
+  document.querySelectorAll("#contact-section > a").forEach((link) => {
+    // A little bit to the right and gray color on hover
+    link.addEventListener("mouseenter", () => {
+      gsap.to(link, {
+        x: "4px",
+        color: "rgb(156 163 175)",
+        duration: 0.2,
+        ease: "power2.out",
+      });
+    });
+    link.addEventListener("mouseleave", () => {
+      gsap.to(link, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
+    });
+  });
+
+  document.querySelectorAll("#socials > a").forEach((link) => {
+    // Turn white on hover
+    link.addEventListener("mouseenter", () => {
+      gsap.to(link, { color: "white", duration: 0.2, ease: "power2.out" });
+    });
+    link.addEventListener("mouseleave", () => {
+      gsap.to(link, { color: "", duration: 0.2, ease: "power2.out" });
+    });
+  });
+
+  // gsap.from("#about-one", {
+  //   scrollTrigger: {
+  //     trigger: "#about-one",
+  //     start: "top bottom",
+  //     end: "bottom top",
+  //     scrub: true,
+  //   },
+  //   x: -300,
+  // });
+
+  const projects = document.querySelectorAll("a.project");
+  const cursor = document.querySelector("#cursor");
+  const cursorText = document.querySelector("#cursor-click");
+
+  let xCTo = gsap.quickTo("#cursor", "left", {
+    duration: 0.3,
+    // ease: "power3",
+  });
+  let yCTo = gsap.quickTo("#cursor", "top", {
+    duration: 0.3,
+    // ease: "power3",
+  });
+  let xDTo = gsap.quickTo("#cursor-click", "left", {
+    duration: 0.4,
+    // ease: "power3",
+  });
+  let yDTo = gsap.quickTo("#cursor-click", "top", {
+    duration: 0.4,
+    // ease: "power3",
+  });
+
+  let mousePosition = { x: 0, y: 0 };
+
+  function moveCursor() {
+    const cursorPosition = {
+      left: mousePosition.x + window.scrollX,
+      top: mousePosition.y + window.scrollY,
+    };
+    xCTo(cursorPosition.left);
+    yCTo(cursorPosition.top);
+    xDTo(cursorPosition.left);
+    yDTo(cursorPosition.top);
+  }
+
+  document.addEventListener("mousemove", (e) => {
+    mousePosition = { x: e.clientX, y: e.clientY };
+    moveCursor();
+  });
+
+  document.addEventListener("scroll", () => {
+    moveCursor();
+  });
+
+  projects.forEach((project) => {
+    const pname = project.querySelector(".pname");
+    const prole = project.querySelector(".prole");
+
+    // Animate the elements on hover
+    project.addEventListener("mouseenter", () => {
+      gsap.to(pname, {
+        x: "-20px",
+        color: "gray",
+        duration: 0.2,
+        ease: "power2.out",
+      });
+      gsap.to(prole, {
+        x: "20px",
+        color: "gray",
+        duration: 0.2,
+        ease: "power2.out",
+      });
+      gsap.to(cursor, {
+        scale: 1,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+      gsap.to(cursorText, {
+        scale: 1,
+        duration: 0.2,
+      });
+      cursor.src = project.querySelector("img").src;
+    });
+
+    project.addEventListener("mouseleave", () => {
+      gsap.to(pname, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
+      gsap.to(prole, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
+      gsap.to(cursor, {
+        scale: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+      gsap.to(cursorText, {
+        scale: 0,
+        duration: 0.2,
+      });
+    });
+  });
 }
-
-document.querySelectorAll("#contact-section > a").forEach((link) => {
-  // A little bit to the right and gray color on hover
-  link.addEventListener("mouseenter", () => {
-    gsap.to(link, {
-      x: "4px",
-      color: "rgb(156 163 175)",
-      duration: 0.2,
-      ease: "power2.out",
-    });
-  });
-  link.addEventListener("mouseleave", () => {
-    gsap.to(link, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
-  });
-});
-
-document.querySelectorAll("#socials > a").forEach((link) => {
-  // Turn white on hover
-  link.addEventListener("mouseenter", () => {
-    gsap.to(link, { color: "white", duration: 0.2, ease: "power2.out" });
-  });
-  link.addEventListener("mouseleave", () => {
-    gsap.to(link, { color: "", duration: 0.2, ease: "power2.out" });
-  });
-});
-
-// gsap.from("#about-one", {
-//   scrollTrigger: {
-//     trigger: "#about-one",
-//     start: "top bottom",
-//     end: "bottom top",
-//     scrub: true,
-//   },
-//   x: -300,
-// });
-
-const projects = document.querySelectorAll("a.project");
-const cursor = document.querySelector("#cursor");
-const cursorText = document.querySelector("#cursor-click");
-
-let xCTo = gsap.quickTo("#cursor", "left", {
-  duration: 0.3,
-  // ease: "power3",
-});
-let yCTo = gsap.quickTo("#cursor", "top", {
-  duration: 0.3,
-  // ease: "power3",
-});
-let xDTo = gsap.quickTo("#cursor-click", "left", {
-  duration: 0.4,
-  // ease: "power3",
-});
-let yDTo = gsap.quickTo("#cursor-click", "top", {
-  duration: 0.4,
-  // ease: "power3",
-});
-
-let mousePosition = { x: 0, y: 0 };
-
-function moveCursor() {
-  const cursorPosition = {
-    left: mousePosition.x + window.scrollX,
-    top: mousePosition.y + window.scrollY,
-  };
-  xCTo(cursorPosition.left);
-  yCTo(cursorPosition.top);
-  xDTo(cursorPosition.left);
-  yDTo(cursorPosition.top);
-}
-
-document.addEventListener("mousemove", (e) => {
-  mousePosition = { x: e.clientX, y: e.clientY };
-  moveCursor();
-});
-
-document.addEventListener("scroll", () => {
-  moveCursor();
-});
-
-projects.forEach((project) => {
-  const pname = project.querySelector(".pname");
-  const prole = project.querySelector(".prole");
-
-  // Animate the elements on hover
-  project.addEventListener("mouseenter", () => {
-    gsap.to(pname, {
-      x: "-20px",
-      color: "gray",
-      duration: 0.2,
-      ease: "power2.out",
-    });
-    gsap.to(prole, {
-      x: "20px",
-      color: "gray",
-      duration: 0.2,
-      ease: "power2.out",
-    });
-    gsap.to(cursor, {
-      scale: 1,
-      duration: 0.5,
-      ease: "power2.out",
-    });
-    gsap.to(cursorText, {
-      scale: 1,
-      duration: 0.2,
-    });
-    cursor.src = project.querySelector("img").src;
-  });
-
-  project.addEventListener("mouseleave", () => {
-    gsap.to(pname, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
-    gsap.to(prole, { x: "0px", color: "", duration: 0.2, ease: "power2.out" });
-    gsap.to(cursor, {
-      scale: 0,
-      duration: 0.5,
-      ease: "power2.out",
-    });
-    gsap.to(cursorText, {
-      scale: 0,
-      duration: 0.2,
-    });
-  });
-});
